c4b97f8f5c195be3974d3b3dc5873f86a11697f2
Erin Highbaugh <erinhighbaugh@gmail.com>
2018-02-07 12:23:47 -0500

    added :after icons to accordions

--- scripts.js
+++ scripts.js
@@ -3,11 +3,7 @@ var i;
 
 for (i = 0; i < acc.length; i++) {
     acc[i].addEventListener("click", function() {
-        /* Toggle between adding and removing the "active" class,
-        to highlight the button that controls the panel */
         this.classList.toggle("active");
-
-        /* Toggle between hiding and showing the active panel */
         var panel = this.nextElementSibling;
         if (panel.style.display === "block") {
             panel.style.display = "none";
@@ -15,16 +11,4 @@ for (i = 0; i < acc.length; i++) {
             panel.style.display = "block";
         }
     });
-}
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var panel = this.nextElementSibling;
-    if (panel.style.maxHeight){
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + "px";
-    } 
-  });
 }
